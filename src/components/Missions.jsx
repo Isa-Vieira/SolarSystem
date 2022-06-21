e2b0e0027b13bc08bdbea3a3c437de7b06050f21
Isabelly Rocha Vieira <isabellyrvieira@icloud.com>
2022-06-21 18:56:42 -0300

    requisito-8-concluido

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions" />
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
+
     );
   }
 }
 
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
 export default Missions;
